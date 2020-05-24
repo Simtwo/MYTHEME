ad9faef97ed48c3b2b3b51648913f7fad9d21e71
Simon BOUCHARD <simonbouchardpro@gmail.com>
2020-05-24 20:06:00 +0200

    Remove plugin filter

--- home.php
+++ home.php
@@ -5,10 +5,30 @@ Template Name: Home
 get_header();
 ?>
 
-<div id="primary" class="content-area">
-      <?php echo do_shortcode('[searchandfilter id="22" show="results"]'); ?>
 
+<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+<div id="primary" class="content-area">
+  <ul class="allarticle">
+
+      <li class="articlehome">
+      <a href="<?php the_permalink(); ?>">
+
+      <?php $image = get_field('image');
+      if( !empty( $image ) ): ?>
+      <div class="illustrations_grande" style="background-image:url('<?php echo esc_url($image['url']); ?>')"></div>
+      <?php endif; ?>
+
+      <div class="overlay" style="display:none;">
+        <h2><?php the_title(); ?></h2>
+      </div>
+        </a>
+      </li>
+
+  </ul>
 </div><!-- #primary -->
 
+
+<?php endwhile; ?>
+<?php endif; ?>
 <?php
 get_footer();
